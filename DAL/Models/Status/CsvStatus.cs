3650c45f8a109148e7cf57358d2c2f7d23c94477
Bogdan Balanyk <balanykb@gmail.com>
2020-06-07 10:08:32 +0300

    scaffold solution skeleton

---
+++ DAL/Models/Status/CsvStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Models.Status
+{
+    internal enum CsvStatus
+    {
+        Approved,
+        Failed,
+        Finished
+    }
+}
---
+++ DAL/Models/Status/XmlStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Models.Status
+{
+    internal enum XmlStatus
+    {
+        Approved,
+        Rejected,
+        Done
+    }
+}
